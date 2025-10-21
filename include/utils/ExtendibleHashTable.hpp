a14cffb5e239a1b0bd93fa3d64dc31e33ccab4ec
Alexandre Menezes de Barros <almdbar@gmail.com>
2025-10-21 17:16:32 -0400

    Preparo do HashTAble Buffer metade

--- include/utils/ExtendibleHashTable.hpp
+++ include/utils/ExtendibleHashTable.hpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <vector>
 #include <cstring>
+#include <unordered_map>
 
 struct HashBucket{
     size_t local_depth;
@@ -23,16 +24,38 @@ private:
     
     // file path and storage variables
     std::string sec_mem_filepath;
-    std::fsteam sec_storage;
+    std::fstream sec_storage;
 
     size_t global_depth;
     size_t bucket_capacity;
     size_t total_records;
-    st
-    
 
-    int insertRecord();
-    int searchRecord();
+
+    std::unordered_map<size_t,HashBucket> bucket_directory;
+    std::unordered_map<size_t,size_t> hash_to_bucket;
+
+    size_t hashFunction(const char* key);
+    size_t getBucketIndex(size_t hash);
+    void doubleDirectory();
+    void splitBucket(size_t bucket_id);
+    void loadBucketFromDisk(size_t bucket_id);
+    void saveBucketToDisk(size_t bucket_id);
+    void saveMetadata();
+    void loadMetadata();
+
+public:
+    ExtensibleHashTable(const std::string& _file_path, size_t _bucket_cap = 4, Logger* _logger);
+    ~ExtensibleHashTable();
+    
+    int insert(const char* key,const std::byte* record_data,size_t record_size);
+    int search(const char* key, std::byte*& record_data, size_t& record_size);
+    int remove(const char* key);
+
+    size_t getRecordCount() const {return total_records; }
+    size_t getGlobalDepth() const {return global_depth; }
+
+    void printStatistics();
+    void reorganize();
 
 }
 
